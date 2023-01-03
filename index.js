20cf31d14ef15b79b2f50b5b2dae8922552cf6f3
Sneha Poulose <snehapoulose1999@gmail.com>
2023-01-03 18:25:34 +0530

    - Three core concepts of redux :            -Store            -Action            -Reducer

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("From index.js");
+// console.log("From index.js");
+const redux = require('redux');
+const createStore = redux.createStore;
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+function orderCake(){                     //action creator
+    return{
+        type:CAKE_ORDERED,
+        quantity:1
+    }
+}
+function restockCake(qua=1) {
+    return{
+        type:CAKE_RESTOCKED,
+        quantity:qua
+    }
+}
+const initalState = {    //store
+    numberOfCakes:10,
+}
+// (prevState,action)=>newState
+const reducer = (state = initalState,action) =>{
+    switch(action.type){
+        case CAKE_ORDERED:
+          return{
+            ...state,
+            numberOfCakes:state.numberOfCakes-1,
+          }
+          case CAKE_RESTOCKED:
+            return{
+                ...state,
+                numberOfCakes:state.numberOfCakes + action.quantity ,
+            }
+          default:
+            return state
+            
+    }
+}
+const store = createStore(reducer) //Holds application state
+console.log("Intial State", store.getState()) // allows access state  via getState()
+ const unsubscribe = store.subscribe(() => console.log("Update State", store.getState()))
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(restockCake(7))
+unsubscribe();
